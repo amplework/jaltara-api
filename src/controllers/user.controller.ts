a20df396c882a2bac16a4fe8203d8b30a6caac3c
Raj <raj@amplework.com>
2024-10-24 16:59:17 +0530

    Update code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,4 @@
 import {authenticate} from '@loopback/authentication';
-import {OPERATION_SECURITY_SPEC} from '@loopback/authentication-jwt';
 import {inject, service} from '@loopback/core';
 import {Filter, FilterExcludingWhere, repository} from '@loopback/repository';
 import {
@@ -22,15 +21,7 @@ import {UserCodeRepository, UserRepository} from '../repositories';
 import {PasswordHasher} from '../services/hash.password.bcryptjs';
 import {TokenService} from '../services/jwt-service';
 import {MyUserService} from '../services/user-service';
-import {validatePassword} from '../services/validator';
-import {
-  ChangePassword,
-  NewUserResponse,
-  otpCredentials,
-  otpCredentialsRequestBody,
-  PasswordChangeRequestBody,
-  UserProfileSchema,
-} from '../utils/type-schema';
+import {NewUserResponse, UserProfileSchema} from '../utils/type-schema';
 
 export class UserController {
   constructor(
@@ -146,141 +137,6 @@ export class UserController {
     };
   }
 
-  @post('/sendOtp', {
-    responses: {
-      '200': {
-        description: 'OTP send response.',
-      },
-    },
-  })
-  async loginWithOTP(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {partial: true}),
-        },
-      },
-    })
-    user: User,
-  ): Promise<any> {
-    if (!user) {
-      throw new HttpErrors.UnprocessableEntity(
-        'To send OTP, Email or Phone is required',
-      );
-    }
-
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    console.log('Generated OTP:', otp);
-    const hashedOtp = await this.passwordHasher.hashPassword(otp);
-
-    const where = {
-      status: 'active',
-      ...(user.email ? {email: user.email} : {}),
-      ...(user.phone ? {phone: user.phone} : {}),
-    };
-
-    const userdata = await this.userRepository.findOne({where});
-    if (!userdata || !userdata.id) {
-      return {
-        statusCode: 404,
-        message: `User not found for ${user.email || user.phone}`,
-      };
-    }
-
-    await this.updateOrCreateUserCode(userdata.id, hashedOtp);
-
-    if (user.email) {
-      console.log('Sending OTP to email service');
-      const dataObj = {otp};
-      const mailOptions = {
-        from: 'no-reply@test.com',
-        to: user.email,
-        slug: 'otp-send',
-        mailContent: dataObj,
-      };
-      // Uncomment when email service is available
-      // return this.emailService.sendMail(mailOptions).then((res) => {
-      //   return {
-      //     statusCode: 200,
-      //     message: `Successfully sent OTP to ${user.email}`,
-      //     role: userdata.role.slug,
-      //   };
-      // }).catch((err) => {
-      //   throw new HttpErrors.UnprocessableEntity(`Error in sending email to ${user.email}`);
-      // });
-    } else if (user.phone) {
-      const userPhone = `${user.countryCode}${user.phone}`;
-      console.log('Sending OTP to phone service');
-      // Uncomment when SMS service is available
-      // await this.taqnyatService.sendOTP(userPhone, otp);
-    }
-
-    return {
-      statusCode: 200,
-      message: `OTP sent successfully`,
-    };
-  }
-
-  async updateOrCreateUserCode(
-    userId: string,
-    hashedOtp: string,
-  ): Promise<void> {
-    const userCodeData = await this.userCodeRepository.findOne({
-      where: {userId},
-    });
-    const expiryMinutes = Number(process.env.CODE_EXPIRY_MINUTES) || 5; // default to 5 minutes if not set
-    const codeExpiry = new Date(new Date().getTime() + expiryMinutes * 60000);
-
-    if (userCodeData && userCodeData.id) {
-      await this.userCodeRepository.updateById(userCodeData.id, {
-        code: hashedOtp,
-        codeExpiry,
-      });
-    } else {
-      await this.userCodeRepository.create({
-        userId,
-        codeExpiry,
-        code: hashedOtp,
-      });
-    }
-  }
-
-  @post('/otp/login', {
-    responses: {
-      '200': {
-        description: 'Token',
-        content: {
-          'application/json': {
-            schema: {
-              type: 'object',
-              properties: {
-                token: {
-                  type: 'string',
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  })
-  async otpLogin(
-    @requestBody(otpCredentialsRequestBody) credentials: otpCredentials,
-  ): Promise<any> {
-    const user = await this.userService.verifyOtpCredentials(credentials);
-    const userProfile = await this.userService.getUserProfile(user);
-    const token = await this.jwtService.generateToken(userProfile);
-    return {
-      statusCode: 200,
-      message: 'Authentication successful',
-      data: {
-        id: user.id,
-        name: user.name,
-        token: token,
-      },
-    };
-  }
-
   @get('/users')
   @response(200, {
     description: 'Array of User model instances',
@@ -295,56 +151,6 @@ export class UserController {
   })
   async find(@param.filter(User) filter?: Filter<User>): Promise<User[]> {
     return this.userRepository.find(filter);
-  }
-
-  @put('/user/change-password', {
-    security: OPERATION_SECURITY_SPEC,
-    responses: {
-      '200': {
-        description: 'The updated user profile',
-        content: {
-          'application/json': {
-            schema: UserProfileSchema,
-          },
-        },
-      },
-    },
-  })
-  @authenticate('jwt')
-  async changePassword(
-    @inject(SecurityBindings.USER) currentUserProfile: UserProfile,
-    @requestBody(PasswordChangeRequestBody) data: ChangePassword,
-  ): Promise<any> {
-    const {oldPassword, password} = data;
-    const {id} = currentUserProfile;
-
-    const user = await this.userRepository.findById(id);
-    if (!user) {
-      throw new HttpErrors.NotFound('User account not found');
-    }
-    const passwordError = await this.userService.validatePassword(password);
-    if (passwordError) {
-      return {
-        success: false,
-        message: passwordError,
-      };
-    }
-
-    validatePassword(oldPassword);
-
-    const isSame = await this.userService.verifyPassword(id, oldPassword);
-    if (isSame) {
-      const passwordHash = await this.passwordHasher.hashPassword(password);
-      await this.userRepository
-        .userCredential(user.id)
-        .patch({password: passwordHash});
-      return {
-        statusCode: 200,
-        message: 'Password changed successfully',
-      };
-    } else {
-      throw new HttpErrors.Unauthorized('Incorrect User credentials');
-    }
   }
 
   @get('/users/{id}')
